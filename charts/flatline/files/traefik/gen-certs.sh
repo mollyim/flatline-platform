6c3c385b2a39ffc437df0433e9b730f2bb2f69c9
Uoemai <218074189+uoemai@users.noreply.github.com>
2025-09-18 15:30:58 +0200

    helm: use custom CA to issue Traefik certificate

---
+++ charts/flatline/files/traefik/gen-certs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Delete previous material.
+rm *.pem *.cer *.srl
+# Create root CA.
+openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:4096 -out ca.key.pem
+chmod 600 ca.key.pem
+openssl req -x509 -new -nodes -key ca.key.pem -sha256 -days 3650 -out ca.pem -config ca.cnf
+# Create wildcard certificate key and CSR.
+openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:4096 -out wildcard-localhost.key.pem
+chmod 600 wildcard-localhost.key.pem
+openssl req -new -key wildcard-localhost.key.pem -out wildcard-localhost.csr -config wildcard-csr.cnf
+# Issue wildcard certificate from CSR with CA.
+openssl x509 -req -in wildcard-localhost.csr -CA ca.pem -CAkey ca.key.pem -CAcreateserial \
+  -out wildcard-localhost.pem -days 3650 -sha256 -extfile wildcard-ext.cnf
+# Generate CA DER file for use with "libsignal". 
+openssl x509 -in ca.pem -outform der -out ca.cer
